867dc957fa67cb6e82af9627ab71fc048243d633
prataplabs <pratapc639@gmail.com>
2026-09-11 20:17:01 +0530

    Time: 1 ms (90.07%) | Memory: 43 MB (15.33%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        
+        if ((n & (n - 1)) != 0) {
+            return false;
+        }
+
+       
+        return (n & 0x55555555) != 0;
+        
+    }
+}
